9d150c1600744c32f731730b309d12b2ff762e20
Pasha Matchak <pavlo@chess.com>
2025-07-13 14:28:59 +0300

    lesson-7

---
+++ lesson-7/outputs.tf
@@ -0,0 +1,44 @@
+output "vpc_id" {
+  description = "ID створеної VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "Список ID публічних підмереж"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "Список ID приватних підмереж"
+  value       = module.vpc.private_subnets
+}
+
+output "internet_gateway_id" {
+  description = "ID Internet Gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "ecr_repository_url" {
+  description = "Повний URL (hostname/імена) для docker push/pull."
+  value       = module.ecr.repository_url
+}
+
+output "ecr_repository_arn" {
+  description = "ARN створеного репозиторію."
+  value       = module.ecr.repository_arn
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint for connecting to the cluster"
+  value       = module.eks.eks_cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.eks_cluster_name
+}
+
+output "eks_node_role_arn" {
+  description = "IAM role ARN for EKS Worker Nodes"
+  value       = module.eks.eks_node_role_arn
+}
